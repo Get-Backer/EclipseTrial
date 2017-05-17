b2415119b96352c0b2ca372ffda1391f3671df6d
Ganesh Padwekar <ganeshpadwekar2989@gmail.com>
2017-05-16 23:10:31 -0400

    adding a comment through eclipse

--- GitDemo/src/com/gitdemo/demo/HelloWorld.java
+++ GitDemo/src/com/gitdemo/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.gitdemo.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		// I am adding comment through eclipse
 		System.out.println("hello world");
 
 	}
